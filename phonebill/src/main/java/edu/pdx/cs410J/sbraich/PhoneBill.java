a98a735b5d098ff64494f103e8775ab59955b556
steve3p0 <stevebpdx@gmail.com>
2018-07-11 09:42:02 -0700

    Added dummy tests.

--- phonebill/src/main/java/edu/pdx/cs410J/sbraich/PhoneBill.java
+++ phonebill/src/main/java/edu/pdx/cs410J/sbraich/PhoneBill.java
@@ -1,4 +1,27 @@
 package edu.pdx.cs410J.sbraich;
 
-public class PhoneBill {
+import edu.pdx.cs410J.AbstractPhoneBill;
+
+import java.util.Collection;
+
+public class PhoneBill extends AbstractPhoneBill<PhoneCall>
+{
+
+    @Override
+    public String getCustomer()
+    {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+
+    @Override
+    public void addPhoneCall(PhoneCall call)
+    {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+
+    @Override
+    public Collection<PhoneCall> getPhoneCalls()
+    {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
 }
---
+++ phonebill/src/test/java/edu/pdx/cs410J/sbraich/PhoneBillTest.java
@@ -0,0 +1,31 @@
+package edu.pdx.cs410J.sbraich;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PhoneBillTest  {
+
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+
+    @Test
+    public void getCustomer() {
+    }
+
+    @Test
+    public void addPhoneCall() {
+    }
+
+    @Test
+    public void getPhoneCalls() {
+    }
+}
