24b95000c2a6fd425169fc4f6b828067ff56021d
steve3p0 <stevebpdx@gmail.com>
2018-08-13 05:27:42 -0700

    Added a new project for the phonebill REST web app

---
+++ phonebill/src/test/java/edu/pdx/cs410J/sbraich/PhoneBillServletTest.java
@@ -0,0 +1,61 @@
+package edu.pdx.cs410J.sbraich;
+
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.*;
+
+/**
+ * A unit test for the {@link PhoneBillServlet}.  It uses mockito to
+ * provide mock http requests and responses.
+ */
+public class PhoneBillServletTest {
+
+  @Test
+  public void initiallyServletContainsNoDictionaryEntries() throws ServletException, IOException {
+    PhoneBillServlet servlet = new PhoneBillServlet();
+
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    PrintWriter pw = mock(PrintWriter.class);
+
+    when(response.getWriter()).thenReturn(pw);
+
+    servlet.doGet(request, response);
+
+    int expectedWords = 0;
+    verify(pw).println(Messages.formatWordCount(expectedWords));
+    verify(response).setStatus(HttpServletResponse.SC_OK);
+  }
+
+  @Test
+  public void addOneWordToDictionary() throws ServletException, IOException {
+    PhoneBillServlet servlet = new PhoneBillServlet();
+
+    String word = "TEST WORD";
+    String definition = "TEST DEFINITION";
+
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    when(request.getParameter("word")).thenReturn(word);
+    when(request.getParameter("definition")).thenReturn(definition);
+
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    PrintWriter pw = mock(PrintWriter.class);
+
+    when(response.getWriter()).thenReturn(pw);
+
+    servlet.doPost(request, response);
+    verify(pw).println(Messages.definedWordAs(word, definition));
+    verify(response).setStatus(HttpServletResponse.SC_OK);
+
+    assertThat(servlet.getDefinition(word), equalTo(definition));
+  }
+
+}
