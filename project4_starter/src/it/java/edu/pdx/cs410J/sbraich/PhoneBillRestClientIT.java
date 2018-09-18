836d4083ef0f6d9e93c9745175c980dd592bd235
steve3p0 <stevebpdx@gmail.com>
2018-09-17 22:08:56 -0700

    initial commit

---
+++ project4_starter/src/it/java/edu/pdx/cs410J/sbraich/PhoneBillRestClientIT.java
@@ -0,0 +1,61 @@
+package edu.pdx.cs410J.sbraich;
+
+import edu.pdx.cs410J.web.HttpRequestHelper;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Integration test that tests the REST calls made by {@link PhoneBillRestClient}
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PhoneBillRestClientIT {
+  private static final String HOSTNAME = "localhost";
+  private static final String PORT = System.getProperty("http.port", "8080");
+
+  private PhoneBillRestClient newPhoneBillRestClient() {
+    int port = Integer.parseInt(PORT);
+    return new PhoneBillRestClient(HOSTNAME, port);
+  }
+
+  @Test
+  public void test0RemoveAllDictionaryEntries() throws IOException {
+    PhoneBillRestClient client = newPhoneBillRestClient();
+    client.removeAllDictionaryEntries();
+  }
+
+  @Test
+  public void test1EmptyServerContainsNoDictionaryEntries() throws IOException {
+    PhoneBillRestClient client = newPhoneBillRestClient();
+    Map<String, String> dictionary = client.getAllDictionaryEntries();
+    assertThat(dictionary.size(), equalTo(0));
+  }
+
+  @Test
+  public void test2DefineOneWord() throws IOException {
+    PhoneBillRestClient client = newPhoneBillRestClient();
+    String testWord = "TEST WORD";
+    String testDefinition = "TEST DEFINITION";
+    client.addDictionaryEntry(testWord, testDefinition);
+
+    String definition = client.getDefinition(testWord);
+    assertThat(definition, equalTo(testDefinition));
+  }
+
+  @Test
+  public void test4MissingRequiredParameterReturnsPreconditionFailed() throws IOException {
+    PhoneBillRestClient client = newPhoneBillRestClient();
+    HttpRequestHelper.Response response = client.postToMyURL();
+    assertThat(response.getContent(), containsString(Messages.missingRequiredParameter("word")));
+    assertThat(response.getCode(), equalTo(HttpURLConnection.HTTP_PRECON_FAILED));
+  }
+
+}
