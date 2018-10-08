3d298f80ea9f487ef5c6bd53f331874f3b651865
steve3p0 <stevebpdx@gmail.com>
2018-10-08 13:39:10 -0700

    initial

---
+++ project5a/phonebill-gwt/src/it/java/edu/pdx/cs410J/sbraich/PhoneBillServiceSyncProxyIT.java
@@ -0,0 +1,35 @@
+package edu.pdx.cs410J.sbraich;
+
+import com.gdevelop.gwt.syncrpc.SyncProxy;
+import edu.pdx.cs410J.sbraich.client.PhoneBill;
+import edu.pdx.cs410J.sbraich.client.PhoneBillService;
+import edu.pdx.cs410J.web.HttpRequestHelper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class PhoneBillServiceSyncProxyIT extends HttpRequestHelper {
+
+  private final int httpPort = Integer.getInteger("http.port", 8080);
+  private String webAppUrl = "http://localhost:" + httpPort + "/phonebill";
+
+  @Test
+  public void gwtWebApplicationIsRunning() throws IOException {
+    Response response = get(this.webAppUrl);
+    assertEquals(200, response.getCode());
+  }
+
+  @Test
+  public void canInvokePhoneBillServiceWithGwtSyncProxy() {
+    String moduleName = "phonebill";
+    SyncProxy.setBaseURL(this.webAppUrl + "/" + moduleName + "/");
+
+    PhoneBillService service = SyncProxy.createSync(PhoneBillService.class);
+    PhoneBill bill = service.getPhoneBill();
+    assertEquals("CS410J", bill.getCustomer());
+    assertEquals(1, bill.getPhoneCalls().size());
+  }
+
+}
---
+++ project5a/phonebill-gwt/src/test/java/edu/pdx/cs410J/sbraich/server/PhoneBillServiceImplTest.java
@@ -0,0 +1,17 @@
+package edu.pdx.cs410J.sbraich.server;
+
+import edu.pdx.cs410J.sbraich.client.PhoneBill;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PhoneBillServiceImplTest {
+
+  @Test
+  public void serviceReturnsExpectedPhoneBill() {
+    PhoneBillServiceImpl service = new PhoneBillServiceImpl();
+    PhoneBill bill = service.getPhoneBill();
+    assertThat(bill.getPhoneCalls().size(), equalTo(1));
+  }
+}
